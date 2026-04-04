3b9bdcf26db9254a211194e9aea71f695fd669f3
Edzel Roque <roque.edzel@gmail.com>
2026-04-04 17:53:39 -0400

    created a barebones structure of the app

---
+++ backend/app.js
@@ -0,0 +1,24 @@
+import express from 'express';
+import session from 'express-session';
+import configRoutes from './routes/index.js';
+
+const app = express();
+
+app.use(express.json()); // allows us to get JSON data
+
+app.use(
+  session({
+    name: 'AuthenticationState',
+    secret: 'some secret string!',
+    resave: false,
+    saveUninitialized: false,
+    cookie: { maxAge: 3600000 },
+  }),
+);
+
+configRoutes(app);
+
+app.listen(3000, () => {
+  console.log("We've now got a server!");
+  console.log('Your routes will be running on http://localhost:3000');
+});
